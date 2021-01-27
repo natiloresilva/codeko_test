93d8db333b73e8cafdeb574f2b3c7c1d97ea227c
Natalia Silva <natalia.pupushi@gmail.com>
2021-01-27 17:40:06 -0300

    bootstrap icons & Delete Fact by ID & Delete All Facts added

--- src/components/FactCard/FactCard.js
+++ src/components/FactCard/FactCard.js
@@ -2,10 +2,15 @@ import React from "react";
 import Card from "react-bootstrap/Card";
 import "./FactCard.css";
 
-const FactCard = ({ category, text }) => {
+const FactCard = ({ category, text, handleDelete }) => {
   return (
     <Card className="card">
-      <Card.Header className="title-card poppins">{category}</Card.Header>
+      <Card.Header className="title-card poppins">
+        {category}
+        <span className="icon" onClick={handleDelete}>
+          <i className="bi bi-file-excel-fill"></i>
+        </span>
+      </Card.Header>
       <Card.Body>
         <blockquote className="blockquote mb-0 inconsolata">
           <p>{text}</p>
--- src/pages/Facts/Facts.js
+++ src/pages/Facts/Facts.js
@@ -20,16 +20,35 @@ const Facts = () => {
       });
   }, [setCategories]);
 
+  useEffect(() => {
+    setFacts(JSON.parse(localStorage.getItem("facts")) || []);
+  }, []);
+
   const getFactCategory = (category) => {
     Axios({
       url: `https://api.chucknorris.io/jokes/random?category=${category}`,
     })
       .then((response) => {
         setFacts([...facts, response.data]);
+        localStorage.setItem(
+          "facts",
+          JSON.stringify([...facts, response.data])
+        );
       })
       .catch((error) => {
         console.error(error);
       });
+  };
+
+  const handleDelete = (key) => {
+    const removeFacts = facts.filter((fact) => fact.id !== key);
+    setFacts(removeFacts);
+    localStorage.setItem("facts", JSON.stringify(removeFacts));
+  };
+
+  const deleteAllFacts = () => {
+    setFacts([]);
+    localStorage.removeItem("facts");
   };
 
   return (
@@ -47,6 +66,16 @@ const Facts = () => {
           </Badge>
         ))}
       </div>
+      <div className="badge-delete-container">
+        <Badge
+          className="badge"
+          pill
+          variant="light"
+          onClick={() => deleteAllFacts()}
+        >
+          Delete All
+        </Badge>
+      </div>
 
       <div className="facts-container">
         <div className="image-facts">
@@ -54,7 +83,12 @@ const Facts = () => {
         </div>
         <div className="facts">
           {facts.map((fact) => (
-            <FactCard category={fact.categories[0]} text={fact.value} />
+            <FactCard
+              key={fact.id}
+              category={fact.categories[0]}
+              text={fact.value}
+              handleDelete={() => handleDelete(fact.id)}
+            />
           ))}
         </div>
         <div className="image-facts">
