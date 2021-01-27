03cdcf21a5e17d61c83ad82ae9f876be680ed159
Natalia Silva <natalia.pupushi@gmail.com>
2021-01-27 12:35:44 -0300

    Get categories & Get Fact by category added

--- src/pages/Facts/Facts.js
+++ src/pages/Facts/Facts.js
@@ -1,7 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Axios from "axios";
+import Badge from "react-bootstrap/Badge";
+import "./Facts.css";
 
 const Facts = () => {
-  return <div>Facts</div>;
-};
+  const [categories, setCategories] = useState([]);
+  const facts = [];
 
+  useEffect(() => {
+    Axios({
+      url: "https://api.chucknorris.io/jokes/categories",
+    })
+      .then((response) => setCategories(response.data))
+      .catch((error) => {
+        console.error(error);
+      });
+  }, [setCategories]);
+
+  const getFactCategory = (category) => {
+    Axios({
+      url: `https://api.chucknorris.io/jokes/random?category=${category}`,
+    })
+      .then((response) => {
+        facts.push(response.data.value);
+        console.log(facts);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  return (
+    <div>
+      <div className="badges-container">
+        {categories.map((category) => (
+          <Badge
+            className="badge"
+            pill
+            variant="secondary"
+            key={category}
+            onClick={() => getFactCategory(category)}
+          >
+            {category}
+          </Badge>
+        ))}
+      </div>
+    </div>
+  );
+};
 export default Facts;
