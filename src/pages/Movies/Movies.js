5615473482de2c9f8b281e75b24c18c6ee9e78b6
Natalia Silva <natalia.pupushi@gmail.com>
2021-01-28 19:04:25 -0300

    Movies page added with React slider and images

--- src/pages/Movies/Movies.js
+++ src/pages/Movies/Movies.js
@@ -1,7 +1,56 @@
 import React from "react";
+import { useState } from "react";
+import Slider from "react-slick";
+import movies from "./moviesArray";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
+import "./Movies.css";
 
-const Movies = () => {
-  return <div>Movies</div>;
-};
+function Movies() {
+  const [imageIndex, setImageIndex] = useState(0);
+
+  const NextArrow = ({ onClick }) => {
+    return (
+      <div className="arrow next" onClick={onClick}>
+        <FaArrowRight />
+      </div>
+    );
+  };
+
+  const PrevArrow = ({ onClick }) => {
+    return (
+      <div className="arrow prev" onClick={onClick}>
+        <FaArrowLeft />
+      </div>
+    );
+  };
+
+  const settings = {
+    infite: true,
+    lazyload: true,
+    speed: 300,
+    slidesToShow: 3,
+    centerMode: true,
+    centerPadding: 0,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
+    beforeChange: (current, next) => setImageIndex(next),
+  };
+
+  return (
+    <div className="container-movies">
+      <div className="container-slider">
+        <Slider {...settings}>
+          {movies.map((img, index) => (
+            <div
+              className={index === imageIndex ? "slide activeSlide" : "slide"}
+            >
+              <img key={index} src={img} alt={img} />
+            </div>
+          ))}
+        </Slider>
+      </div>
+    </div>
+  );
+}
 
 export default Movies;
---
+++ src/pages/Movies/moviesArray.js
@@ -0,0 +1,31 @@
+import deadlyReunion from "../../assets/images/movies/deadly-reunion.jpg";
+import expendables from "../../assets/images/movies/expendables2.jpg";
+import forceOfOne from "../../assets/images/movies//force-of-one.jpg";
+import goodGuys from "../../assets/images/movies/good-guys-wear-black.jpg";
+import logans from "../../assets/images/movies/logans-war.jpg";
+import martialLaw from "../../assets/images/movies/martial-law.jpg";
+import presidentsMan from "../../assets/images/movies/presidents-man.jpg";
+import presidentsMan2 from "../../assets/images/movies/presidents-man2.jpg";
+import slaughter from "../../assets/images/movies/slaughter-in-san-francisco-1.jpg";
+import cutter from "../../assets/images/movies/the-cutter.jpg";
+import trial from "../../assets/images/movies/trial-by-fire.jpg";
+import walkerTexas from "../../assets/images/movies/walker-texas.jpg";
+import dragon from "../../assets/images/movies/way-of-the-dragon.jpg";
+
+const images = [
+  deadlyReunion,
+  expendables,
+  forceOfOne,
+  goodGuys,
+  logans,
+  martialLaw,
+  presidentsMan,
+  presidentsMan2,
+  slaughter,
+  cutter,
+  trial,
+  walkerTexas,
+  dragon,
+];
+
+export default images;
